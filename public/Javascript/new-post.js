7ed4884e0774de8c7ec5280b3692ee137d2ed5ad
OmarD <omar.daghestani@colorado.edu>
2022-10-29 17:22:43 -0600

    testing the new-post feature

--- public/Javascript/new-post.js
+++ public/Javascript/new-post.js
@@ -1,12 +1,12 @@
 async function Submit() {
-    const response = await fetch('/api/dashboard/new-post', {
+    const response = await fetch('/api/dashboard', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' }
     });
     console.log(response);
     
     if (response.ok) {
-        document.location.replace('/dashboard/');
+        document.location.replace('/');
         } else {
         alert("Please fill out all fields");
         }
